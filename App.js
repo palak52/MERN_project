69db2794ecc6b0b3e37a70946156ce67eac4fcf3
Palak Vaishnav <61025576+palak52@users.noreply.github.com>
2022-12-23 09:10:30 +0530

    frontEnd

---
+++ App.js
@@ -0,0 +1,49 @@
+// import{BrowserRouter as Router,Route,Routes}from 'react-router-dom';
+// import './App.css';
+
+// import CreateBook from './components/CreateBook.js';
+// import ShowBookList from './components/ShowBookList';
+// import ShowBookDetails from './components/ShowBookDetails';
+// import UpdateBookInfo from './components/UpdateBookInfo';
+
+// function App() {
+//   return (
+//     <Routes>
+//       <div>
+//         <Route exact path='/' element = {<ShowBookList/>}></Route>
+//         <Route path='/create-book' element={<CreateBook/>}></Route>
+//         <Route path='/edit-book/:id' element={<UpdateBookInfo/>}></Route>
+//         <Route path='/show-book/:id' element={<ShowBookDetails/>}></Route>
+//       </div>
+//     </Routes>
+
+
+    
+//   );
+// }
+
+// export default App;
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+
+import CreateBook from './components/CreateBook';
+import ShowBookList from './components/ShowBookList';
+import ShowBookDetails from './components/ShowBookDetails';
+import UpdateBookInfo from './components/UpdateBookInfo';
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path='/' element={<ShowBookList />} />
+          <Route path='/create-book' element={<CreateBook />} />
+          <Route path='/edit-book/:id' element={<UpdateBookInfo />} />
+          <Route path='/show-book/:id' element={<ShowBookDetails />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
